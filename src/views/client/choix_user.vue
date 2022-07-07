fc98fe0f0e8ea020e42354384e345aae33eb2104
Gautier LEHERICY <gautier.lehericy@groupe-esigelec.org>
2022-07-07 09:59:02 +0200

    feat(email)

--- src/views/client/choix_user.vue
+++ src/views/client/choix_user.vue
@@ -1,37 +1,92 @@
 <template>
-  <div class="utilisateur-wrapper">
-    <div class="utilisateur-inner">
-      <h1>Particulier ou entreprise</h1>
-    </div>
+  <div class="container">
+      <label>Name</label>
+      <input
+          type="text"
+          v-model="templateData.name"
+          name="name"
+          placeholder="Your Name"
+      >
+      <label>Email</label>
+      <input
+          type="email"
+          v-model="templateData.email"
+          name="email"
+          placeholder="Your Email"
+      >
+      <button @click="submit">Envoyer</button>
   </div>
 </template>
 
 <script>
+import emailjs from 'emailjs-com';
 //import nav_component from '../../components/nav-component/nav-component.vue'
 export default {
-    components:{},
-    name:'choix_user'
-
+    components:{
+    },
+    name:'choix_user',
+    data() {
+      return {
+        templateData: {
+          name: '',
+          email: ''
+        }
+      }
+    },
+    methods: {
+      submit() {
+        console.log(this.templateData)
+        try {
+          emailjs.send('service_c80epto', 'template_tlczdng', this.templateData, 'wOtWU47pMC9aqC3Wz');
+        } catch(error) {
+          console.log({error})
+        }
+        // Reset form field
+        this.templateData.name = ''
+        this.templateData.email = ''
+      },
+    }
 }
 </script>
 
 <style scoped>
-  .utilisateur-wrapper{
-    display: flex;
-    margin-top: 50px;
-    justify-content: center;
-    flex-direction:column;
-    text-align:left;
-  }
+* {box-sizing: border-box;}
 
-  .utilisateur-inner{
-    width: 60%;
-    height: 1200px;
-    margin:auto;
-    background: #ffffff;
-    box-shadow: 0px 14px 80px rgba(34,35,58,0.2);
-    padding: 40px 55px 45px 55px;
-    border-radius: 15px;
-    transition: all .3s;
-  }
+.container {
+  display: block;
+  margin:auto;
+  text-align: center;
+  border-radius: 5px;
+  background-color: #f2f2f2;
+  padding: 20px;
+  width: 50%;
+}
+
+label {
+  float: left;
+}
+
+input[type=text], [type=email], textarea {
+  width: 100%;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  margin-top: 6px;
+  margin-bottom: 16px;
+  resize: vertical;
+}
+
+input[type=submit] {
+  background-color: #4CAF50;
+  color: white;
+  padding: 12px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type=submit]:hover {
+  background-color: #45a049;
+}
 </style>
